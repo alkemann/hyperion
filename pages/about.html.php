51394c13f35319840c068a20ce8bc1bc8c9816e9
Alexander Morland <alek.morland@gmail.com>
2022-04-10 13:28:33 +0200

    replaced home and about

--- pages/about.html.php
+++ pages/about.html.php
@@ -3,16 +3,15 @@ $faker = Faker\Factory::create();
 $company = $faker->company();
 ?>
 <h2 class="text-xl mb-2">About <?=$company?></h2>
-<hr>
 <p>Here at <strong><?=$company?></strong> we believe in <strong><?=$faker->bs()?></strong>
 to maximize our ability to develiver <strong><?=$faker->catchPhrase()?></strong>!
 </p>
 <p><?=$faker->realText()?></p>
 <h3 class="mt-3">Meet the team!</h3>
-<ul class="my-2 ml-3 list-disc list-inside">
-  <li>CEO <?=$faker->firstName()?> <?=$faker->lastName()?></li>
+<ul class="mt-2 mb-6 ml-3 list-disc list-inside">
+  <li><strong><?=$faker->firstName()?></strong> CEO</li>
   <?php for($i = 0 ; $i < 5; $i++)  : ?>
-  <li><?=$faker->jobTitle()?> <?=$faker->firstName()?> <?=$faker->lastName()?></li>
+  <li><strong><?=$faker->firstName()?></strong> <?=$faker->jobTitle()?></li>
   <?php endfor; ?>
 </ul>
 <p><?=$faker->realText()?></p>
--- pages/home.html.php
+++ pages/home.html.php
@@ -1,11 +1,10 @@
 <?php $faker = Faker\Factory::create(); ?>
 
-<h2 class="text-xl mb-2">Home</h2>
+<h2 class="text-xl mb-2">Hyper</h2>
+<p>This is an app to test out and demonstrate  a HTML focused way, using HTMX, Apline.js, Tailwind ad H2L</p>
 <hr>
-<div>
-  <dl>
-    <dt>faker->name()</dt><dd><?=$faker->name()?></dd>
-    <dt>faker->email()</dt><dd><?=$faker->email()?></dd>
-    <dt>faker->text()</dt><dd><?=$faker->catchPhrase()?></dd>
-</dl>
-</div>
+<h3>Features</h3>
+<ol>
+  <li>Partial page content (header and sidebar says when that makes sense)</li>
+  <li>A todo list?</li>
+</ol>
