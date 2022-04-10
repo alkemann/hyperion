bf380f647705544c92611628e4d2c25974331da8
Alexander Morland <alek.morland@gmail.com>
2022-04-10 19:30:42 +0200

    fixed typos

--- layouts/default/neck.html.php
+++ layouts/default/neck.html.php
@@ -1,6 +1,6 @@
 <header class="">
   <h1 class="px-2"><a href="/">Hyperion</a></h1>
-  <h5 class="text-sm px-2">A HTML focused way, using HTMX, Apline.js, Tailwind ad H2L</h5>
+  <h5 class="text-sm px-2">A HTML focused way, using HTMX, Apline.js, Tailwind and H2L.</h5>
   <?php echo $this->part('navbar')?>
 </header>
 <div id="main-wrapper" class="flex gap-6 flex-row-reverse mt-4 mx-2">
--- pages/home.html.php
+++ pages/home.html.php
@@ -1,7 +1,7 @@
 <?php $faker = Faker\Factory::create(); ?>
 
 <h2 class="text-xl mb-2">Hyper</h2>
-<p>This is an app to test out and demonstrate  a HTML focused way, using HTMX, Apline.js, Tailwind ad H2L</p>
+<p>This is an app to test out and demonstrate  a HTML focused way, using HTMX, Apline.js, Tailwind and H2L</p>
 <hr>
 <h3>Features</h3>
 <ol>
