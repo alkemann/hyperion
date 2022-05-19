8ed80002b1b1b740cd00d178a429a2f6d8926789
Alexander Morland <alek.morland@gmail.com>
2022-05-19 14:10:44 +0200

    dont use more for sidebar news

--- parts/sidebar.html.php
+++ parts/sidebar.html.php
@@ -2,7 +2,7 @@
 <div id="side-bar" class="border p-4 text-sm max-w-xs">
   <?php $faker = Faker\Factory::create(); ?>
   <h3 class="text-xl mb-4"><a href="/news">News</a></h3>
-  <div hx-swap="this" hx-get="/news/more" hx-trigger="every 30s">
+  <div hx-swap="this" hx-get="/news/sidebar" hx-trigger="every 20s">
   <?php for($i = 0 ; $i < 3; $i++) echo $this->part('/news/card'); ?>
   </div>
   <hr>
