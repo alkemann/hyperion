6367d8193b5fb5ee1f8f5a4f7d5389513bbe993f
Alexander Morland <alek.morland@gmail.com>
2022-05-20 15:37:30 +0200

    logging in connections

--- config/connections.php
+++ config/connections.php
@@ -1,11 +1,15 @@
 <?php
 
-use alkemann\h2l\Connections;
+use alkemann\h2l\{ Connections, Log };
 use alkemann\h2l\data\PDO;
 
-$db_url = getenv('NF_POSTGRESQL_POSTGRES_URI') ??  getenv('DATABASE_URL') ?? false;
+$db_url = getenv('NF_POSTGRESQL_POSTGRES_URI');
+if (!$db_url) $db_url = getenv('DATABASE_URL');
 if ($db_url) {
+    Log::debug(print_r($db_url, true));
     $options = parse_url($db_url);
     $options['db'] = ltrim($options['path'], '/');
     Connections::add('default', function() use ($options) { return new PDO($options); });
+} else {
+    Log::error("NO DB CONNECTED");
 }
