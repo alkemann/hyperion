131d0d55814373cc7fb455d42ee84227eed4ddeb
Alexander Morland <alek.morland@gmail.com>
2022-05-20 15:44:17 +0200

    fixed scheme in connnections and no layout in error page

--- config/connections.php
+++ config/connections.php
@@ -6,9 +6,10 @@ use alkemann\h2l\data\PDO;
 $db_url = getenv('NF_POSTGRESQL_POSTGRES_URI');
 if (!$db_url) $db_url = getenv('DATABASE_URL');
 if ($db_url) {
-    Log::debug(print_r($db_url, true));
     $options = parse_url($db_url);
     $options['db'] = ltrim($options['path'], '/');
+    $options['scheme'] = 'pgsql';
+    Log::debug(print_r($options, true));
     Connections::add('default', function() use ($options) { return new PDO($options); });
 } else {
     Log::error("NO DB CONNECTED");
--- pages/error.html.php
+++ pages/error.html.php
@@ -1,2 +1,3 @@
+<?php $this->layout = false; ?>
 <h2>ERROR - Sowwy</h2>
 <p><?php echo $message ?></p>
